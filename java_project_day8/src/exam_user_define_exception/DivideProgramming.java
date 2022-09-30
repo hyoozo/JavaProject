dcd9ade82c9f0c36be00a5913e319981104bdf7b
hyoozo <eo_8282@naver.com>
2022-09-30 15:18:24 +0900

    9월 30일 수업-3

--- java_project_day8/src/exam_user_define_exception/DivideProgramming.java
+++ java_project_day8/src/exam_user_define_exception/DivideProgramming.java
@@ -2,18 +2,16 @@ package exam_user_define_exception;
 
 //사용자 정의 예외 클래스 선언
 @SuppressWarnings("serial")
-class DivideByZoroException extends Exception {
-    // class DivideByZreoException extends ArithmeticException { // class 사용자 정의 예외 클래스 extends 예외
+class DivideByZeroException extends Exception {
+    // class DivideByZeroException extends ArithmeticException { // class 사용자 정의 예외 클래스 extends 예외
     private String msg;
 
-    public DivideByZoroException(){
+    public DivideByZeroException(){
         //super ("0으로 나눌수는 없음.");
     }
-
-    public DivideByZoroException(String msg) {
+    public DivideByZeroException(String msg) {
         this.msg = msg;
     }
-
     @Override
     public String getMessage() {
         if (msg == null) {
@@ -23,22 +21,24 @@ class DivideByZoroException extends Exception {
         }
     }
 }
-
-
 public class DivideProgramming {
     public static void main(String[] args) {
         double result;
         try {
             result = quotient(1, 0);
             System.out.println(result);
-        } catch (DivideByZoroException e) {
+        } catch (DivideByZeroException e) {
             System.out.println(e.getMessage());
 //            System.out.println(e.toString());
         }
     }
-    public static double quotient (int n, int d) throws DivideByZoroException {
+    public static double quotient (int n, int d) throws DivideByZeroException {
         if ( d == 0 )// 예외상황
-            throw new DivideByZoroException(); //예외 객체 생성
+            throw new DivideByZeroException(); //예외 객체 생성
         return (double) n/d;
     }
 }
+/*
+     console
+0으로 나눌수는 없음.
+ */
